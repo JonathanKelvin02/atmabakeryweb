5bc8ef83d22637087cb70046372800d073b4bec0
WIBU1234 <210711186@students.uajy.ac.id>
2024-05-24 10:12:31 +0700

    Pusing

--- src/Pages/CustomerView/HomepageView/ContentHomepageView.jsx
+++ src/Pages/CustomerView/HomepageView/ContentHomepageView.jsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import PatternImage from '../../../assets/Pattern.svg';
 import LapisLegit from '../../../assets/Homepage/LapisLegit.svg';
+import LearnMoreImage from '../../../assets/Homepage/LearnMore.svg';
+import PropTypes from 'prop-types';
 
+// Import CSS
 import './ContentHomepageView.css';
 
 function ContentHomepageView() {
@@ -10,29 +14,34 @@ function ContentHomepageView() {
 
     return (
         <>
-            <div className='OuterContainer'>
-                <div className='TopMiddleContent'>
-                    <img src={PatternImage} className='top-left-image' alt='Image 1' />
-                    <img src={PatternImage} className='bottom-right-image' alt='Image 2' />
+            <div className={`mainSectionContent`}>
+                <div className="imageGroup">
+                    <img className="heroImage" src={PatternImage} alt="alt text" />
+                    <img className="bannerImage" src={PatternImage} alt="alt text" />
 
-                    <Row className='bottom-row'>
-                        <Col className="responsive-colForTopText">
-                            <h3 className="responsive-text-h3" style={{ fontWeight: 700, fontSize: '2.5vw', fontFamily: 'Roboto, sans-serif' }}>
+                    <div className="contentRow">
+                        <div className="contentColumn">
+                            <h1 className="mainHeaderContent">
                                 Welcome from our greatest heart, where every pastry is crafted with passion to bring joy to your table
-                            </h3>
+                            </h1>
+                            <h1 className="mainParagContent">
+                                Welcome to AtmaBakery! Here, we combine generations of bakery expertise with modern innovations to deliver
+                                breads and cakes that are not only delicious but also enticing. Every bite is the result of carefully
+                                selected finest ingredients and a manufacturing process full of love and precision.
+                            </h1>
 
-                            <p className="responsive-text-p" style={{ fontFamily: 'Roboto, sans-serif', fontSize: '1.5vw', marginTop: "30px" }}>
-                                Welcome to AtmaBakery! Here, we combine generations of bakery expertise with modern innovations to deliver breads and cakes that are not only delicious but also enticing. Every bite is the result of carefully selected finest ingredients and a manufacturing process full of love and precision.
-                            </p>
-                        </Col>
-                        <Col className="responsive-col" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                            <img src={LapisLegit} alt='Image Lapis' style={{ width: "22vw", height: "auto", minWidth: "200px" }} />
-                        </Col>
-                    </Row>
+                            <div className="learnMoreGroup">
+                                <img className="iconImage" src={LearnMoreImage} alt="alt text" />
+                                <h1 className="learnMoreTitle">Learn More</h1>
+                            </div>
+                        </div>
+
+                        <img className="decorativeImage" src={LapisLegit} alt="alt text" />
+                    </div>
                 </div>
             </div>
         </>
-    );
+    );    
 }
 
 export default ContentHomepageView;
